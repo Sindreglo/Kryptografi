c93be6180d9fa7d58b21762f57340d30577e841f
Sindre <sindre.glomnes@gmail.com>
2022-09-28 11:47:44 +0200

    exercise4 oppgave2 cpp

---
+++ exercise4/oppgave2_cpp/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <regex>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string word;
+
+  cout << "Word: ";
+  // cin >> word;
+  word = "<test&test>";
+
+  word = regex_replace(word, regex("&"), "&amp"); // replace 'def' -> 'klm'
+  word = regex_replace(word, regex("<"), "&lt");
+  word = regex_replace(word, regex(">"), "&gt");
+  cout << "New word: " << word << endl;
+}
